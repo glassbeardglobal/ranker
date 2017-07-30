6c538540d3cfbe27771ab161929c4d5c168493d5
Jonathan Xu <jonathanj.xu97@gmail.com>
2017-07-29 22:20:21 -0400

    Home page, first draft

--- webapp/src/scenes/Home/index.js
+++ webapp/src/scenes/Home/index.js
@@ -4,13 +4,22 @@ import SignupForm from 'components/SignupForm';
 require('./home.css');
 
 const Home = () => (
-  <div className="home container col-wrapper">
-    <div className="text-main col-8">
-      <h1>Organize your ideas with Ranker</h1>
-    </div>
+  <div className="home">
+    <div className="container col-wrapper">
+      <div className="col-7">
+        <div className="text-main">
+          <h1>Organize your thoughts</h1>
+          <p>Ranker&#39;s platform empowers you to prioritize ideas in an easy and intuitive way</p>
+        </div>
+      </div>
 
-    <div className="col-4">
-      <SignupForm />
+      <div className="col-1" />
+
+      <div className="col-4">
+        <div className="signup-form-wrapper">
+          <SignupForm />
+        </div>
+      </div>
     </div>
   </div>
 );
