44a92d6c5727bd5ab6fea1a09d8191ea0920ee8a
Jonathan Xu <jonathanj.xu97@gmail.com>
2017-07-30 23:55:09 -0400

    Move cleanWebpackPlugin to prod

--- conf/webpack.prod.js
+++ conf/webpack.prod.js
@@ -1,7 +1,21 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
 const config = require('./webpack.config.js');
 
 const extension = {
   devtool: 'source-map'
 };
 
-module.exports = Object.assign({}, extension, config);
+const prodConfig = Object.assign({}, extension, config);
+
+// Clears webapp/build folder
+const cleanWebpack = new CleanWebpackPlugin([
+    path.resolve(__dirname, '..', 'webapp', 'build')
+  ], {
+    root: path.resolve(__dirname, '..', 'webapp')
+  });
+
+prodConfig.plugins.splice(0, 0, cleanWebpack);
+
+module.exports = prodConfig;
